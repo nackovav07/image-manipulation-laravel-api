8a6e5444ef5158acaa4243fc46f85e9d10a85c7d
Verica <vnackova07@gmail.com>
2023-03-23 23:17:13 +0100

    First Part

---
+++ app/Http/Controllers/AlbumController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAlbumRequest;
+use App\Http\Requests\UpdateAlbumRequest;
+use App\Models\Album;
+
+class AlbumController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Album::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAlbumRequest $request)
+    {
+        $album = Album::create($request->all());
+
+        return $album;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Album $album)
+    {
+        return $album;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAlbumRequest $request, Album $album)
+    {
+        $album->update($request->all());
+
+        return $album;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Album $album)
+    {
+        $album->delete();
+
+        return response('', 204);
+    }
+}
